c3fb8f70ffb0a55c2ff38297cd26c9ab1736a061
hokyunrhee <hokyun.rhee@gmail.com>
2021-05-03 00:18:17 +0900

    chore: Icon 폴더 삭제

--- src/components/atoms/Icon/index.tsx
+++
@@ -1,5 +0,0 @@
-export { default as Pencil } from './Pencil';
-export { default as Me } from './Me';
-export { default as Home } from './Home';
-export { default as Moon } from './Moon';
-export { default as Sun } from './Sun';
